6bcc529011f3eadd2b5b37f9fa6680180b5370fb
Declan Zhang <85047300+Adehit@users.noreply.github.com>
2024-05-01 22:26:34 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+const characters = [
+    { "name": "黄炫铭", "identity": "学长", "school": "北京大学", "keyProfile": "尽人事，听天命！" },
+    { "name": "郑凯祥", "identity": "学长", "school": "清华大学", "keyProfile": "再读一遍题目。" },
+    { "name": "极品老师", "identity": "老师", "school": "莆田一中", "keyProfile": "你告诉我怎么配？！" },
+    { "name": "戴冠哲", "identity": "学长", "school": "待补充", "keyProfile": "高考语文139。" },
+    { "name": "陈诺", "identity": "学姐", "school": "待补充", "keyProfile": "高考跪在17题。" },
+    { "name": "吴天然", "identity": "老师", "school": "莆田一中", "keyProfile": "我见都不敢见。" },
+    { "name": "23届的学生", "identity": "学生", "school": "天南海北", "keyProfile": "最不痛心的一届。" }
+];
