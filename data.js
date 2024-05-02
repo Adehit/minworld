94fa955a4d3706cfb49fb5872b0e1607bf51ec28
Declan Zhang <85047300+Adehit@users.noreply.github.com>
2024-05-02 10:53:21 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,9 +1,14 @@
 const characters = [
-    { "name": "黄炫铭", "identity": "学长", "school": "北京大学", "keyProfile": "尽人事，听天命！" },
-    { "name": "郑凯祥", "identity": "学长", "school": "清华大学", "keyProfile": "再读一遍题目。" },
-    { "name": "极品老师", "identity": "老师", "school": "莆田一中", "keyProfile": "你告诉我怎么配？！" },
-    { "name": "戴冠哲", "identity": "学长", "school": "待补充", "keyProfile": "高考语文139。" },
-    { "name": "陈诺", "identity": "学姐", "school": "待补充", "keyProfile": "高考跪在17题。" },
-    { "name": "吴天然", "identity": "老师", "school": "莆田一中", "keyProfile": "我见都不敢见。" },
-    { "name": "23届的学生", "identity": "学生", "school": "天南海北", "keyProfile": "最不痛心的一届。" }
+    { "name": "黄炫铭", "identity": "学长", "school": "北京大学", "keyProfile": "尽人事，听天命" },
+    { "name": "郑凯祥", "identity": "学长", "school": "清华大学", "keyProfile": "再读一遍题目" },
+    { "name": "极品老师", "identity": "老师", "school": "莆田一中", "keyProfile": "你告诉我怎么配" },
+    { "name": "戴冠哲", "identity": "学长", "school": "待补充", "keyProfile": "高考语文139" },
+    { "name": "陈诺", "identity": "学姐", "school": "待补充", "keyProfile": "高考跪在17题" },
+    { "name": "吴天然", "identity": "老师", "school": "莆田一中", "keyProfile": "我见都不敢见" },
+    { "name": "23届的学生", "identity": "学生", "school": "天南海北", "keyProfile": "最不痛心的一届" },
+    { "name": "20届那个学生", "identity": "学生", "school": "待补充", "keyProfile": "对我恨之入骨" },
+    { "name": "湖南长郡中学", "identity": "学校", "school": "长郡中学", "keyProfile": "草稿纸比我们的答题卡还干净" },
+    { "name": "厦门一中", "identity": "学校", "school": "厦门一中", "keyProfile": "[龙头三连击]" },
+    { "name": "龙岩一中", "identity": "学校", "school": "龙岩一中", "keyProfile": "我才明白，什么才是兄弟学校" },
+    { "name": "六中的一个老师", "identity": "老师", "school": "六中", "keyProfile": "&#8757和因为的区别你知道吗" }
 ];
